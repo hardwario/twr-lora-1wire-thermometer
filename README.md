17833ea98a542156de410af6f3c2e0671b8bc051
hubmartin <hub.martin@gmail.com>
2018-05-16 14:34:20 +0200

    Fix readme

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
 <a href="https://www.bigclown.com"><img src="https://s3.eu-central-1.amazonaws.com/bigclown/gh-readme-logo.png" alt="BigClown Logo" align="right"></a>
 
-# Firmware For Scissor Lift
+# Wireless pool temperature sensor firmware
 
 [![Travis](https://img.shields.io/travis/bigclownlabs/bc-core-module/master.svg)](https://travis-ci.org/bigclownlabs/bcf-scissor-lift-node)
 [![Release](https://img.shields.io/github/release/bigclownlabs/bc-core-module.svg)](https://github.com/bigclownlabs/bcf-scissor-lift-node/releases)
 [![License](https://img.shields.io/github/license/bigclownlabs/bc-core-module.svg)](https://github.com/bigclownlabs/bcf-scissor-lift-node/blob/master/LICENSE)
 [![Twitter](https://img.shields.io/twitter/follow/BigClownLabs.svg?style=social&label=Follow)](https://twitter.com/BigClownLabs)
-
-This repository contains firmware for scissor lift node that measure temperature and every 10 seconds communicated with generic gateway. Using CP201t thermistor.
-
-
-Herda Marek:
-Vyčítání třeba každých 30sekund by bylo OK. Informaci o téměř vybité baterii bych uvítal zakódováním do teploty, třeba speciálním číslem teploty, řekněme 0.99 stupnu 3x za sebou, třeba s periodou každých 6 hodin. Šlo by to takto? Zjednodušilo by mi to alerting.
-
-
-
 
 If you want to get more information about Core Module, firmware and how to work with it, please follow this link:
 
--- app/application.c
+++ app/application.c
@@ -1,21 +1,51 @@
 #include <application.h>
 #include <bc_ds18b20.h>
 
-#define UPDATE_INTERVAL (30 * 1000)
+// Time after the sending is less frequent to save battery
+#define SERVICE_INTERVAL_INTERVAL (60 * 60 * 1000)
+#define BATTERY_UPDATE_INTERVAL   (10 * 60 * 1000)
 
-#define BATTERY_UPDATE_INTERVAL (10 * 60 * 1000)
+#define UPDATE_SERVICE_INTERVAL            (5 * 1000)
+#define UPDATE_NORMAL_INTERVAL             (10 * 1000)
+#define BAROMETER_UPDATE_SERVICE_INTERVAL  (1 * 60 * 1000)
+#define BAROMETER_UPDATE_NORMAL_INTERVAL   (5 * 60 * 1000)
+
+#define TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL (5 * 60 * 1000)
+#define TEMPERATURE_TAG_PUB_VALUE_CHANGE 0.2f
+
+#define HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL (5 * 60 * 1000)
+#define HUMIDITY_TAG_PUB_VALUE_CHANGE 5.0f
+
+#define LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL (5 * 60 * 1000)
+#define LUX_METER_TAG_PUB_VALUE_CHANGE 25.0f
+
+#define BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL (5 * 60 * 1000)
+#define BAROMETER_TAG_PUB_VALUE_CHANGE 20.0f
 
 static bc_led_t led;
-
 static bc_button_t button;
-
 static bc_ds18b20_t ds18d20;
+// Thermometer instance
+bc_tmp112_t tmp112;
+
+struct {
+    event_param_t temperature;
+    event_param_t temperature_ds18b20;
+    event_param_t humidity;
+    event_param_t illuminance;
+    event_param_t pressure;
+
+} params;
 
 void handler_button(bc_button_t *s, bc_button_event_t e, void *p);
 
 void handler_battery(bc_module_battery_event_t e, void *p);
 
 void handler_ds18b20(bc_ds18b20_t *s, bc_ds18b20_event_t e, void *p);
+
+void climate_module_event_handler(bc_module_climate_event_t event, void *event_param);
+
+void switch_to_normal_mode_task(void *param);
 
 void application_init(void)
 {
@@ -33,9 +63,23 @@ void application_init(void)
 
     bc_ds18b20_init(&ds18d20, BC_DS18B20_RESOLUTION_BITS_12);
     bc_ds18b20_set_event_handler(&ds18d20, handler_ds18b20, NULL);
-    bc_ds18b20_set_update_interval(&ds18d20, UPDATE_INTERVAL);
+    bc_ds18b20_set_update_interval(&ds18d20, UPDATE_SERVICE_INTERVAL);
 
-    bc_radio_pairing_request("tpca-scissor-lift-node", VERSION);
+    // Initialize thermometer sensor on core module
+    bc_tmp112_init(&tmp112, BC_I2C_I2C0, 0x49);
+
+    // Initialize climate module
+    bc_module_climate_init();
+    bc_module_climate_set_event_handler(climate_module_event_handler, NULL);
+    bc_module_climate_set_update_interval_thermometer(UPDATE_SERVICE_INTERVAL);
+    bc_module_climate_set_update_interval_hygrometer(UPDATE_SERVICE_INTERVAL);
+    bc_module_climate_set_update_interval_lux_meter(UPDATE_SERVICE_INTERVAL);
+    bc_module_climate_set_update_interval_barometer(BAROMETER_UPDATE_NORMAL_INTERVAL);
+    bc_module_climate_measure_all_sensors();
+
+    bc_scheduler_register(switch_to_normal_mode_task, NULL, SERVICE_INTERVAL_INTERVAL);
+
+    bc_radio_pairing_request("kit-wireless-pool-sensor", VERSION);
 
     bc_led_pulse(&led, 2000);
 }
@@ -75,12 +119,98 @@ void handler_ds18b20(bc_ds18b20_t *s, bc_ds18b20_event_t e, void *p)
 {
     (void) p;
 
-    float temperature = NAN;
+    float value = NAN;
 
     if (e == bc_ds18b20_EVENT_UPDATE)
     {
-        bc_ds18b20_get_temperature_celsius(s, &temperature);
-    }
+        bc_ds18b20_get_temperature_celsius(s, &value);
 
-    bc_radio_pub_temperature(BC_RADIO_PUB_CHANNEL_A, &temperature);
+        if ((fabs(value - params.temperature_ds18b20.value) >= TEMPERATURE_TAG_PUB_VALUE_CHANGE) || (params.temperature_ds18b20.next_pub < bc_scheduler_get_spin_tick()))
+        {
+            bc_radio_pub_temperature(BC_RADIO_PUB_CHANNEL_A, &value);
+            params.temperature_ds18b20.value = value;
+            params.temperature_ds18b20.next_pub = bc_scheduler_get_spin_tick() + TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL;
+        }
+    }
 }
+
+void switch_to_normal_mode_task(void *param)
+{
+    bc_module_climate_set_update_interval_thermometer(UPDATE_NORMAL_INTERVAL);
+    bc_module_climate_set_update_interval_hygrometer(UPDATE_NORMAL_INTERVAL);
+    bc_module_climate_set_update_interval_lux_meter(UPDATE_NORMAL_INTERVAL);
+    bc_module_climate_set_update_interval_barometer(BAROMETER_UPDATE_SERVICE_INTERVAL);
+
+    bc_ds18b20_set_update_interval(&ds18d20, UPDATE_NORMAL_INTERVAL);
+
+    bc_scheduler_unregister(bc_scheduler_get_current_task_id());
+}
+
+void climate_module_event_handler(bc_module_climate_event_t event, void *event_param)
+{
+    (void) event_param;
+
+    float value;
+
+    if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_THERMOMETER)
+    {
+        if (bc_module_climate_get_temperature_celsius(&value))
+        {
+            if ((fabs(value - params.temperature.value) >= TEMPERATURE_TAG_PUB_VALUE_CHANGE) || (params.temperature.next_pub < bc_scheduler_get_spin_tick()))
+            {
+                bc_radio_pub_temperature(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
+                params.temperature.value = value;
+                params.temperature.next_pub = bc_scheduler_get_spin_tick() + TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+    else if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_HYGROMETER)
+    {
+        if (bc_module_climate_get_humidity_percentage(&value))
+        {
+            if ((fabs(value - params.humidity.value) >= HUMIDITY_TAG_PUB_VALUE_CHANGE) || (params.humidity.next_pub < bc_scheduler_get_spin_tick()))
+            {
+                bc_radio_pub_humidity(BC_RADIO_PUB_CHANNEL_R3_I2C0_ADDRESS_DEFAULT, &value);
+                params.humidity.value = value;
+                params.humidity.next_pub = bc_scheduler_get_spin_tick() + HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+    else if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_LUX_METER)
+    {
+        if (bc_module_climate_get_illuminance_lux(&value))
+        {
+            if (value < 1)
+            {
+                value = 0;
+            }
+            if ((fabs(value - params.illuminance.value) >= LUX_METER_TAG_PUB_VALUE_CHANGE) || (params.illuminance.next_pub < bc_scheduler_get_spin_tick()) ||
+                    ((value == 0) && (params.illuminance.value != 0)) || ((value > 1) && (params.illuminance.value == 0)))
+            {
+                bc_radio_pub_luminosity(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
+                params.illuminance.value = value;
+                params.illuminance.next_pub = bc_scheduler_get_spin_tick() + LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+    else if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_BAROMETER)
+    {
+        if (bc_module_climate_get_pressure_pascal(&value))
+        {
+            if ((fabs(value - params.pressure.value) >= BAROMETER_TAG_PUB_VALUE_CHANGE) || (params.pressure.next_pub < bc_scheduler_get_spin_tick()))
+            {
+                float meter;
+
+                if (!bc_module_climate_get_altitude_meter(&meter))
+                {
+                    return;
+                }
+
+                bc_radio_pub_barometer(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value, &meter);
+                params.pressure.value = value;
+                params.pressure.next_pub = bc_scheduler_get_spin_tick() + BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+}
+
