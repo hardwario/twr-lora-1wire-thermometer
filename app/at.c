11897ee9dd7ed21d8c4b476ae32ce096f9b86fa6
hubmartin <hub.martin@gmail.com>
2021-10-12 13:26:40 +0200

    Update prefixes to TWR

--- app/at.c
+++ app/at.c
@@ -3,16 +3,16 @@
 
 static struct
 {
-    bc_led_t *led;
-    bc_cmwx1zzabz_t *lora;
+    twr_led_t *led;
+    twr_cmwx1zzabz_t *lora;
     char tmp[36];
 
 } _at;
 
-static bool _at_param_eui_test(bc_atci_param_t *param);
-static bool _at_param_key_test(bc_atci_param_t *param);
+static bool _at_param_eui_test(twr_atci_param_t *param);
+static bool _at_param_key_test(twr_atci_param_t *param);
 
-void at_init(bc_led_t *led, bc_cmwx1zzabz_t *lora)
+void at_init(twr_led_t *led, twr_cmwx1zzabz_t *lora)
 {
     _at.led = led;
     _at.lora = lora;
@@ -20,136 +20,136 @@ void at_init(bc_led_t *led, bc_cmwx1zzabz_t *lora)
 
 bool at_deveui_read(void)
 {
-    bc_cmwx1zzabz_get_deveui(_at.lora, _at.tmp);
+    twr_cmwx1zzabz_get_deveui(_at.lora, _at.tmp);
 
-    bc_atci_printf("$DEVEUI: %s", _at.tmp);
+    twr_atci_printfln("$DEVEUI: %s", _at.tmp);
 
     return true;
 }
 
-bool at_deveui_set(bc_atci_param_t *param)
+bool at_deveui_set(twr_atci_param_t *param)
 {
     if (!_at_param_eui_test(param))
     {
         return false;
     }
 
-    bc_cmwx1zzabz_set_deveui(_at.lora, param->txt);
+    twr_cmwx1zzabz_set_deveui(_at.lora, param->txt);
 
     return true;
 }
 
 bool at_devaddr_read(void)
 {
-    bc_cmwx1zzabz_get_devaddr(_at.lora, _at.tmp);
+    twr_cmwx1zzabz_get_devaddr(_at.lora, _at.tmp);
 
-    bc_atci_printf("$DEVADDR: %s", _at.tmp);
+    twr_atci_printfln("$DEVADDR: %s", _at.tmp);
 
     return true;
 }
 
-bool at_devaddr_set(bc_atci_param_t *param)
+bool at_devaddr_set(twr_atci_param_t *param)
 {
 
-    bc_cmwx1zzabz_set_devaddr(_at.lora, param->txt);
+    twr_cmwx1zzabz_set_devaddr(_at.lora, param->txt);
 
     return true;
 }
 
 bool at_nwkskey_read(void)
 {
-    bc_cmwx1zzabz_get_nwkskey(_at.lora, _at.tmp);
+    twr_cmwx1zzabz_get_nwkskey(_at.lora, _at.tmp);
 
-    bc_atci_printf("$NWKSKEY: %s", _at.tmp);
+    twr_atci_printfln("$NWKSKEY: %s", _at.tmp);
 
     return true;
 }
 
-bool at_nwkskey_set(bc_atci_param_t *param)
+bool at_nwkskey_set(twr_atci_param_t *param)
 {
     if (!_at_param_key_test(param))
     {
         return false;
     }
 
-    bc_cmwx1zzabz_set_nwkskey(_at.lora, param->txt);
+    twr_cmwx1zzabz_set_nwkskey(_at.lora, param->txt);
 
     return true;
 }
 
 bool at_appkey_read(void)
 {
-    bc_cmwx1zzabz_get_appkey(_at.lora, _at.tmp);
+    twr_cmwx1zzabz_get_appkey(_at.lora, _at.tmp);
 
-    bc_atci_printf("$APPKEY: %s", _at.tmp);
+    twr_atci_printfln("$APPKEY: %s", _at.tmp);
 
     return true;
 }
 
-bool at_appkey_set(bc_atci_param_t *param)
+bool at_appkey_set(twr_atci_param_t *param)
 {
     if (!_at_param_key_test(param))
     {
         return false;
     }
 
-    bc_cmwx1zzabz_set_appkey(_at.lora, param->txt);
+    twr_cmwx1zzabz_set_appkey(_at.lora, param->txt);
 
     return true;
 }
 
 bool at_appeui_read(void)
 {
-    bc_cmwx1zzabz_get_appeui(_at.lora, _at.tmp);
+    twr_cmwx1zzabz_get_appeui(_at.lora, _at.tmp);
 
-    bc_atci_printf("$APPEUI: %s", _at.tmp);
+    twr_atci_printfln("$APPEUI: %s", _at.tmp);
 
     return true;
 }
 
-bool at_appeui_set(bc_atci_param_t *param)
+bool at_appeui_set(twr_atci_param_t *param)
 {
     if (!_at_param_eui_test(param))
     {
         return false;
     }
 
-    bc_cmwx1zzabz_set_appeui(_at.lora, param->txt);
+    twr_cmwx1zzabz_set_appeui(_at.lora, param->txt);
 
     return true;
 }
 
 bool at_appskey_read(void)
 {
-    bc_cmwx1zzabz_get_appskey(_at.lora, _at.tmp);
+    twr_cmwx1zzabz_get_appskey(_at.lora, _at.tmp);
 
-    bc_atci_printf("$APPSKEY: %s", _at.tmp);
+    twr_atci_printfln("$APPSKEY: %s", _at.tmp);
 
     return true;
 }
 
-bool at_appskey_set(bc_atci_param_t *param)
+bool at_appskey_set(twr_atci_param_t *param)
 {
     if (!_at_param_key_test(param))
     {
         return false;
     }
 
-    bc_cmwx1zzabz_set_appskey(_at.lora, param->txt);
+    twr_cmwx1zzabz_set_appskey(_at.lora, param->txt);
 
     return true;
 }
 
 bool at_band_read(void)
 {
-    bc_cmwx1zzabz_config_band_t band = bc_cmwx1zzabz_get_band(_at.lora);
+    twr_cmwx1zzabz_config_band_t band = twr_cmwx1zzabz_get_band(_at.lora);
 
-    bc_atci_printf("$BAND: %d", band);
+    twr_atci_printfln("$BAND: %d", band);
 
     return true;
 }
 
-bool at_band_set(bc_atci_param_t *param)
+bool at_band_set(twr_atci_param_t *param)
 {
     uint8_t band = atoi(param->txt);
 
@@ -158,21 +158,21 @@ bool at_band_set(bc_atci_param_t *param)
         return false;
     }
 
-    bc_cmwx1zzabz_set_band(_at.lora, band);
+    twr_cmwx1zzabz_set_band(_at.lora, band);
 
     return true;
 }
 
 bool at_mode_read(void)
 {
-    bc_cmwx1zzabz_config_mode_t mode = bc_cmwx1zzabz_get_mode(_at.lora);
+    twr_cmwx1zzabz_config_mode_t mode = twr_cmwx1zzabz_get_mode(_at.lora);
 
-    bc_atci_printf("$MODE: %d", mode);
+    twr_atci_printfln("$MODE: %d", mode);
 
     return true;
 }
 
-bool at_mode_set(bc_atci_param_t *param)
+bool at_mode_set(twr_atci_param_t *param)
 {
     uint8_t mode = atoi(param->txt);
 
@@ -181,28 +181,28 @@ bool at_mode_set(bc_atci_param_t *param)
         return false;
     }
 
-    bc_cmwx1zzabz_set_mode(_at.lora, mode);
+    twr_cmwx1zzabz_set_mode(_at.lora, mode);
 
     return true;
 }
 
 bool at_join(void)
 {
-    bc_cmwx1zzabz_join(_at.lora);
+    twr_cmwx1zzabz_join(_at.lora);
 
     return true;
 }
 
 bool at_nwk_read(void)
 {
-    uint8_t nwk_public = bc_cmwx1zzabz_get_nwk_public(_at.lora);
+    uint8_t nwk_public = twr_cmwx1zzabz_get_nwk_public(_at.lora);
 
-    bc_atci_printf("$NWK: %d", nwk_public);
+    twr_atci_printfln("$NWK: %d", nwk_public);
 
     return true;
 }
 
-bool at_nwk_set(bc_atci_param_t *param)
+bool at_nwk_set(twr_atci_param_t *param)
 {
     uint8_t nwk_public = atoi(param->txt);
 
@@ -211,19 +211,19 @@ bool at_nwk_set(bc_atci_param_t *param)
         return false;
     }
 
-    bc_cmwx1zzabz_set_nwk_public(_at.lora, nwk_public);
+    twr_cmwx1zzabz_set_nwk_public(_at.lora, nwk_public);
 
     return true;
 }
 
 bool at_blink(void)
 {
-    bc_led_blink(_at.led, 3);
+    twr_led_blink(_at.led, 3);
 
     return true;
 }
 
-bool at_led_set(bc_atci_param_t *param)
+bool at_led_set(twr_atci_param_t *param)
 {
     if (param->length != 1)
     {
@@ -232,14 +232,14 @@ bool at_led_set(bc_atci_param_t *param)
 
     if (param->txt[0] == '1')
     {
-        bc_led_set_mode(_at.led, BC_LED_MODE_ON);
+        twr_led_set_mode(_at.led, TWR_LED_MODE_ON);
 
         return true;
     }
 
     if (param->txt[0] == '0')
     {
-        bc_led_set_mode(_at.led, BC_LED_MODE_OFF);
+        twr_led_set_mode(_at.led, TWR_LED_MODE_OFF);
 
         return true;
     }
@@ -249,12 +249,12 @@ bool at_led_set(bc_atci_param_t *param)
 
 bool at_led_help(void)
 {
-    bc_atci_printf("$LED: (0,1)");
+    twr_atci_printfln("$LED: (0,1)");
 
     return true;
 }
 
-static bool _at_param_eui_test(bc_atci_param_t *param)
+static bool _at_param_eui_test(twr_atci_param_t *param)
 {
     if (param->length != 16)
     {
@@ -275,7 +275,7 @@ static bool _at_param_eui_test(bc_atci_param_t *param)
 }
 
 
-static bool _at_param_key_test(bc_atci_param_t *param)
+static bool _at_param_key_test(twr_atci_param_t *param)
 {
     if (param->length != 32)
     {
--- app/at.h
+++ app/at.h
@@ -17,38 +17,38 @@
 #define AT_LED_COMMANDS {"$BLINK", at_blink, NULL, NULL, NULL, "LED blink 3 times"},\
                         {"$LED", NULL, at_led_set, NULL, at_led_help, "LED on/off"}
 
-void at_init(bc_led_t *led, bc_cmwx1zzabz_t *lora);
+void at_init(twr_led_t *led, twr_cmwx1zzabz_t *lora);
 
 bool at_deveui_read(void);
-bool at_deveui_set(bc_atci_param_t *param);
+bool at_deveui_set(twr_atci_param_t *param);
 
 bool at_devaddr_read(void);
-bool at_devaddr_set(bc_atci_param_t *param);
+bool at_devaddr_set(twr_atci_param_t *param);
 
 bool at_nwkskey_read(void);
-bool at_nwkskey_set(bc_atci_param_t *param);
+bool at_nwkskey_set(twr_atci_param_t *param);
 
 bool at_appskey_read(void);
-bool at_appskey_set(bc_atci_param_t *param);
+bool at_appskey_set(twr_atci_param_t *param);
 
 bool at_appkey_read(void);
-bool at_appkey_set(bc_atci_param_t *param);
+bool at_appkey_set(twr_atci_param_t *param);
 
 bool at_appeui_read(void);
-bool at_appeui_set(bc_atci_param_t *param);
+bool at_appeui_set(twr_atci_param_t *param);
 
 bool at_band_read(void);
-bool at_band_set(bc_atci_param_t *param);
+bool at_band_set(twr_atci_param_t *param);
 
 bool at_mode_read(void);
-bool at_mode_set(bc_atci_param_t *param);
+bool at_mode_set(twr_atci_param_t *param);
 
 bool at_nwk_read(void);
-bool at_nwk_set(bc_atci_param_t *param);
+bool at_nwk_set(twr_atci_param_t *param);
 
 bool at_join(void);
 bool at_blink(void);
-bool at_led_set(bc_atci_param_t *param);
+bool at_led_set(twr_atci_param_t *param);
 bool at_led_help(void);
 
 #endif // _AT_H
